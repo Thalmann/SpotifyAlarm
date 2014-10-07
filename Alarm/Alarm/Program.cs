5ce01a6a68560184bb509a7aa7079fe0000013c6
Bruno Thalmann <mr.thalmann@gmail.com>
2014-10-07 10:25:43 +0200

    conenction to spotify works

--- Alarm/Alarm/Program.cs
+++ Alarm/Alarm/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using SpotifyAPI.SpotifyLocalAPI;
 
 namespace Alarm
 {
@@ -10,6 +12,19 @@ namespace Alarm
     {
         static void Main(string[] args)
         {
+            SpotifyLocalAPIClass spotify = new SpotifyLocalAPIClass();
+            if (!spotify.Connect())
+            {
+                Console.WriteLine("Spotify is not running");
+                Thread.Sleep(3000);
+                return;
+            }
+
+            SpotifyMusicHandler musichHandler = spotify.GetMusicHandler();
+
+            
+            musichHandler.Play();
+
         }
     }
 }
