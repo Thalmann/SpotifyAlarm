77c34cfac15d5cbcccd28ea858f54cfb0da60fd0
Bruno Thalmann <mr.thalmann@gmail.com>
2014-10-23 17:20:19 +0200

    added hour and minute to add date GUI control

--- Alarm/Alarm/GUI/AddAlarm.Designer.cs
+++ Alarm/Alarm/GUI/AddAlarm.Designer.cs
@@ -30,11 +30,13 @@
         {
             this.addAlarmButton = new System.Windows.Forms.Button();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBoxHour = new System.Windows.Forms.TextBox();
+            this.textBoxMinute = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // addAlarmButton
             // 
-            this.addAlarmButton.Location = new System.Drawing.Point(209, 3);
+            this.addAlarmButton.Location = new System.Drawing.Point(271, 2);
             this.addAlarmButton.Name = "addAlarmButton";
             this.addAlarmButton.Size = new System.Drawing.Size(75, 23);
             this.addAlarmButton.TabIndex = 0;
@@ -48,15 +50,34 @@
             this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
             this.dateTimePicker1.TabIndex = 1;
             // 
+            // textBoxHour
+            // 
+            this.textBoxHour.Location = new System.Drawing.Point(209, 5);
+            this.textBoxHour.Name = "textBoxHour";
+            this.textBoxHour.Size = new System.Drawing.Size(25, 20);
+            this.textBoxHour.TabIndex = 2;
+            this.textBoxHour.Text = "12";
+            // 
+            // textBoxMinute
+            // 
+            this.textBoxMinute.Location = new System.Drawing.Point(240, 5);
+            this.textBoxMinute.Name = "textBoxMinute";
+            this.textBoxMinute.Size = new System.Drawing.Size(25, 20);
+            this.textBoxMinute.TabIndex = 3;
+            this.textBoxMinute.Text = "00";
+            // 
             // AddAlarm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.textBoxMinute);
+            this.Controls.Add(this.textBoxHour);
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.addAlarmButton);
             this.Name = "AddAlarm";
-            this.Size = new System.Drawing.Size(287, 28);
+            this.Size = new System.Drawing.Size(351, 28);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -64,5 +85,7 @@
 
         private System.Windows.Forms.Button addAlarmButton;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBoxHour;
+        private System.Windows.Forms.TextBox textBoxMinute;
     }
 }
--- Alarm/Alarm/GUI/MainForm.Designer.cs
+++ Alarm/Alarm/GUI/MainForm.Designer.cs
@@ -30,9 +30,9 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.addAlarmControl = new MusicAlarm.GUI.AddAlarm();
             this.listBox = new System.Windows.Forms.ListBox();
+            this.addAlarmControl = new MusicAlarm.GUI.AddAlarm();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.SuspendLayout();
@@ -60,30 +60,30 @@
             this.tabPage1.Text = "tabPage1";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
-            // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(192, 74);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // addAlarmControl
-            // 
-            this.addAlarmControl.Location = new System.Drawing.Point(8, 22);
-            this.addAlarmControl.Name = "addAlarmControl";
-            this.addAlarmControl.Size = new System.Drawing.Size(287, 28);
-            this.addAlarmControl.TabIndex = 0;
-            // 
             // listBox
             // 
             this.listBox.FormattingEnabled = true;
             this.listBox.Location = new System.Drawing.Point(8, 65);
             this.listBox.Name = "listBox";
-            this.listBox.Size = new System.Drawing.Size(287, 147);
+            this.listBox.Size = new System.Drawing.Size(352, 147);
             this.listBox.TabIndex = 1;
+            // 
+            // addAlarmControl
+            // 
+            this.addAlarmControl.Location = new System.Drawing.Point(8, 22);
+            this.addAlarmControl.Name = "addAlarmControl";
+            this.addAlarmControl.Size = new System.Drawing.Size(352, 28);
+            this.addAlarmControl.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(427, 244);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
